6a5554a6a271478a96e0f5c3c276cf78d0183157
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2024-10-27 12:44:20 +0300

    ex3.c

--- ex3.c
+++ ex3.c
@@ -4,11 +4,9 @@
 int main() {
     char pet[10];
 
-    // Ask the user if they have a cat or a dog
     printf("Do you have a cat or a dog? ");
     scanf("%s", pet);
 
-    // Use if statements to determine the sound the pet makes
     if (strcmp(pet, "cat") == 0) {
         printf("Meow!\n");
     } else if (strcmp(pet, "dog") == 0) {
