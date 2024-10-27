a6dbe5ebe57bbd3129ea808f29e9c04adb22740d
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2024-10-27 12:46:47 +0300

    ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+void sayHello(char name[]);
+
+int main() {
+    char name[50];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    sayHello(name);
+
+    return 0;
+}
+
+void sayHello(char name[]) {
+    printf("Hello, %s!\n", name);
+}
