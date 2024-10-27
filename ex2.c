16df0ecc9316c663c952c406e0254c93f32b6b81
Saed Abdul Rahim <151928187+SaedAbdulRahim@users.noreply.github.com>
2024-10-27 12:39:41 +0300

    ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+
+  printf("Enter your age: ");
+    scanf("%d", &age);
+
+    for (int i = 0; i < age; i++) {
+        printf("Happy Birthday!\n");
+    }
+    return 0;
+}
